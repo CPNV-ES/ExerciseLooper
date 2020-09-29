bfd9a0a4913733aaaee2e5aee0feffacd042f597
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-09-29 14:34:59 +0200

    have select template request

--- _db/Req.sql
+++ _db/Req.sql
@@ -12,3 +12,27 @@ VALUES(NULL,'TITLE',types,idExercice)
 #exemple : INSERT into exercices VALUES(NULL,'Questions1',1 ,1);
 
 
+
+DELETE FROM exercices
+WHERE exercices.title = "";
+
+
+UPDATE maTable SET xxxxx = "xxxxx" 
+WHERE maTable.id = id;
+
+#exemple : UPDATE exercises SET title = "hello world" WHERE exercises.idExercise = id;
+
+
+
+SELECT * 
+FROM maTable 
+INNER JOIN monAutreTable ON monAutreTable.FK_idMaTable = maTable.id 
+WHERE maTable.id  = 1
+
+#exemple SELECT * FROM exercises INNER JOIN questions ON questions.FK_idExercises = exercises.idExercises WHERE exercises.idExercises = 1
+
+
+
+
+ 
+
