0254420a12cce48604ec1b3e5807fbfc9bdad691
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-06 15:02:17 +0200

    Added CRUD for responses

---
+++ model/response.php
@@ -0,0 +1,54 @@
+<?php 
+
+class response{
+
+    public $id;
+    public $content;
+    public $FK_idQuestions;
+
+    public static function createResponse($content)
+    {
+        $db = db::connect();
+        $req = "INSERT INTO `responses` VALUES(`idResponses`,?)";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($content));
+    }
+    
+    public static function getByIdQuestion($id)
+    {
+        $db = db::connect();
+        $req = "SELECT `idResponses`,`content`,`FK_idQuestions` FROM `responses` WHERE FK_idQuestions = ?";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($id));
+        $result = $stmt->fetchAll();
+
+        foreach ($result as $Key) {
+
+            $que = new response();
+            $que->id = $Key['idResponses'];
+            $que->content = $Key['content'];
+            $array[] = clone $que;
+        }
+        return $array;
+    }
+
+    public static function updateById($id,$editedContent)
+    {
+        $db = db::connect();
+        $req = "UPDATE `responses` SET content=?, WHERE idResponses = ? ";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($editedContent,$id));
+    }
+    
+    private static function deleteResponseById($id)
+    {
+        $db = db::connect();
+        $req = "DELETE FROM `responses` WHERE idResponses = ?";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($id));
+    }
+}
+
+
+
+?>
