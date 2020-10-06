5160d811734a6e4654e8e2816929187e4d4f260c
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-06 14:50:02 +0200

    Added basic function CRUD

--- model/question.php
+++ model/question.php
@@ -12,7 +12,7 @@ class question{
     public static function getByIdExercises($id)
     {
         $db = db::connect();
-        $req = "SELECT `idQuestions`,`title`,`type` FROM `questions` WHERE FK_idExercises = ?";
+        $req = "SELECT `idQuestions`,`title`,`type`,`FK_idExercises` FROM `questions` WHERE FK_idExercises = ?";
         $stmt = $db->prepare($req);
         $stmt->execute(array($id));
         $result = $stmt->fetchAll();
@@ -20,9 +20,10 @@ class question{
         foreach ($result as $Key) {
 
             $que = new question();
-            $que->id = $Key['idExercises'];
+            $que->id = $Key['idQuestions'];
             $que->title = $Key['title'];
             $que->type= $Key['type'];
+            $que->FK_idExercises= $Key['FK_idExercises'];
             $array[] = clone $que;
         }
         return $array;
@@ -34,6 +35,23 @@ class question{
         $req = "UPDATE `questions` SET title=?,type=?  WHERE idQuestions = ? ";
         $stmt = $db->prepare($req);
         $stmt->execute(array($editedTitle,$editedType,$id));
+    }
+
+    
+    public static function createQuestion($title,$type,$FK_idExercises)
+    {
+        $db = db::connect();
+        $req = "INSERT INTO `questions` VALUES(`idQuestions`,?,?,?)";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($title,$type,$FK_idExercises));
+    }
+
+    public static function deleteQuestionById($id)
+    {
+        $db = db::connect();
+        $req = "DELETE FROM `questions` WHERE idQuestions = ?";
+        $stmt = $db->prepare($req);
+        $stmt->execute(array($id));
     }
 
 }
