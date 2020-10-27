1807d10d3f020c17991f090bdfd2100c8be08c24
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-27 15:01:03 +0100

    Added route

--- index.php
+++ index.php
@@ -1,39 +1,50 @@
 <?php
-
+session_start();
 
 
 define('SITE_PATH', realpath(dirname(__FILE__)));
-$site_url = str_replace('\\', '/', str_replace(realpath( $_SERVER['DOCUMENT_ROOT']), '', SITE_PATH));
-define('SITE_URL', 'http://'.$_SERVER['HTTP_HOST']. $site_url);
+$site_url = str_replace('\\', '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', SITE_PATH));
+define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST'] . $site_url);
 
 require_once SITE_PATH . "/controller/exerciseController.php";
+require_once SITE_PATH . "/controller/questionController.php";
 
 ob_start();
 
 $request = explode("?", $_SERVER['REQUEST_URI'], 2);
 $next = isset($_GET['next']) ? $_GET['next'] : false;
-
-
 switch ($request[0]) {
     default:
         http_response_code(404);
         errorPage404();
         break;
     case '':
-    case '/' :
+    case '/':
         homePage();
         break;
-    case '/create' :
+    case '/create':
         createExercise();
         break;
-    case '/take' :
+    case '/take':
         takeExercise();
         break;
-    case '/manage' :
+    case '/manage':
         manageExercise();
         break;
-    case '/newExercise' :
+    case '/newExercise':
         newExercise();
+        break;
+
+    case '/createQuestion':
+        createQuestion();
+        break;
+
+    case '/displayExercise':
+        displayExercise($_GET['id']);
+        break;
+
+    case '/deleteQuestion':
+        deleteQuestion();
         break;
 }
 
