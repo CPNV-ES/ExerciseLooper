c963396da7b4e5620b37ce45e039a838a4654cbd
Sou <souphakone.samoutphonh@cpnv.ch>
2020-10-29 15:00:41 +0100

    addes end bracket

--- controller/exerciseController.php
+++ controller/exerciseController.php
@@ -3,53 +3,53 @@ require_once "model/exercise.php";
 
 class exerciseController
 {
-// Affichage de la page de l'accueil
-function homePage()
-{
-    require "view/homeView.php";
-}
+    // Affichage de la page de l'accueil
+    function homePage()
+    {
+        require "view/homeView.php";
+    }
 
-function takeExercise()
-{
-    require "view/takeExerciseView.php";
-}
+    function takeExercise()
+    {
+        require "view/takeExerciseView.php";
+    }
 
-function createExercise()
-{
-    require "view/createExerciseView.php";
-}
+    function createExercise()
+    {
+        require "view/createExerciseView.php";
+    }
 
-function manageExercise()
-{
-    require "view/showExerciseView.php";
-}
+    function manageExercise()
+    {
+        require "view/showExerciseView.php";
+    }
 
-function errorPage404()
-{
-    require "view/errorPage404.php";
-}
-function destroyExercise()
-{
+    function errorPage404()
+    {
+        require "view/errorPage404.php";
+    }
+    function destroyExercise()
+    {
 
-    
-}
+        
+    }
 
-function newExercise()
-{
-    $value = $_POST['exercise']['title'];
-    $idExercise = exercise::createExercise($value);
-    $_SESSION['exId'] = $idExercise;
-    require "view/fieldExerciseView.php";
-}
+    function newExercise()
+    {
+        $value = $_POST['exercise']['title'];
+        $idExercise = exercise::createExercise($value);
+        $_SESSION['exId'] = $idExercise;
+        require "view/fieldExerciseView.php";
+    }
 
-function displayExercise($id)
-{
-    $ex = new exercise();
-    $ex->getById($id);
-    $questions = question::getByIdExercises($ex->id);
-    $_SESSION['questions'] =  $questions;
-    $_SESSION['exId'] = $id;
-    require "view/fieldExerciseView.php";
+    function displayExercise($id)
+    {
+        $ex = new exercise();
+        $ex->getById($id);
+        $questions = question::getByIdExercises($ex->id);
+        $_SESSION['questions'] =  $questions;
+        $_SESSION['exId'] = $id;
+        require "view/fieldExerciseView.php";
+    }
 }
-
 ?>
