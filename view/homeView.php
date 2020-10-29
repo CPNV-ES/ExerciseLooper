ed34a8efe25c5e5e502fa84b8702c3d22b341ec4
Rossier Gabriel <Rossier.GABRIEL@cpnv.ch>
2020-10-29 15:01:37 +0100

    Working button with new route

--- view/homeView.php
+++ view/homeView.php
@@ -7,13 +7,13 @@
     <div class="container dashboard">
         <section class="row">
             <div class="column">
-                <a class="button answering column" href="/take">Take an exercise</a>
+                <a class="button answering column" href="/?controller=exercise&action=takeExercise">Take an exercise</a>
             </div>
             <div class="column">
-                <a class="button managing column" href="/create">Create an exercise</a>
+                <a class="button managing column" href="/?controller=exercise&action=createExercise">Create an exercise</a>
             </div>
             <div class="column">
-                <a class="button results column" href="/manage">Manage an exercise</a>
+                <a class="button results column" href="/?controller=exercise&action=manageExercise">Manage an exercise</a>
             </div>
         </section>
     </div>
