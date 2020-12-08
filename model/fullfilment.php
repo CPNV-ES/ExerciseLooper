6483de8e06da847ef4f6541d93809e8884479471
William <william.hausmann@cpnv.ch>
2020-12-08 14:51:32 +0100

    update the model for fullfilments
    Refactor a function 
    Refactor the constructor

--- model/fullfilment.php
+++ model/fullfilment.php
@@ -1,41 +1,39 @@
 <?php
 
-include_once "db/db.php";
+include_once "db.php";
+
 class fullfilments{
 
     public $id;
     public $date;
     public $FK_idExercises;
-    public $FK_idResponses;
 
-    public  function __construct($id,$date,$fk_ex,$fk_resp)
+    public  function __construct($id,$date,$fk_ex)
     {
         $this->id = $id;
         $this->date = $date;
         $this->FK_idExercises = $fk_ex;
-        $this->FK_idResponses = $fk_resp;
     }
 
-    public static function create($idExercise,$idResponses)
+    public static function create($idExercise)
     {
         $db = db::connect();
         $date = date("Y.m.d","hh.mm.ss");
-        $req = "INSERT INTO `fullfilments` VALUES(`idExercises`,?,?,?)";
+        $req = "INSERT INTO `fullfilments` VALUES(`idExercises`,?,?)";
         $stmt = $db->prepare($req);
-        $stmt->execute(array($date,$idExercise,$idResponses));
+        $stmt->execute(array($date,$idExercise));
     }
 
-    public static function getAll()
-    {
+    public static function getByIdExercise($id){
         $db = db::connect();
-        $req = "SELECT `idFullfilments`,`date`,`FK_idExercises`,`FK_idResponses` FROM `fullfilments`";
+        $req = "SELECT `idFullfilment`, `date`,`FK_idExercises` FROM `fullfilments` WHERE FK_idExercises = ?";
         $stmt = $db->prepare($req);
-        $stmt->execute();
+        $stmt->execute(array($id));
         $result = $stmt->fetchAll();
 
         foreach ($result as $Key) {
 
-            $ex = new fullfilments($Key['idFullfilments'],$Key['date'],$Key['FK_idExercises'],$Key['FK_idResponses']);
+            $ex = new fullfilments($Key['idFullfilment'],$Key['date'],$Key['FK_idExercises']);
             $array[] = clone $ex;
         }
         return $array;
