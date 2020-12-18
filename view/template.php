ae792b3620061de4830c227e99c9928cd3d7608c
William <william.hausmann@cpnv.ch>
2020-12-18 22:17:33 +0100

    Favicon

--- view/template.php
+++ view/template.php
@@ -6,6 +6,7 @@
 <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 <head>
     <title>ExerciseLooper</title>
+    <link rel="icon" type="image/png" href="../content/img/favicon.png"/>
     <link rel="stylesheet" media="all" href="../content/css/stylesheet.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <script src="../content/js/script.js"></script>
